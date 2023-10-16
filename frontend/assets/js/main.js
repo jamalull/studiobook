02c76565980f2b07d961fcace793cffd098fb96f
Muhammadrizqi11 <mhmmdrizqi2002@gmail.com>
2023-10-16 15:34:33 +0700

    menambahkan landing page

---
+++ frontend/assets/js/main.js
@@ -0,0 +1,41 @@
+let menuOpen = document.querySelector(".menu-toggle");
+let menuWrapper = document.querySelector(".menu-wrapper");
+
+menuOpen.addEventListener("click", function () {
+  menuOpen.classList.toggle("bx-x");
+  menuOpen.classList.toggle("bxs-grid");
+  menuWrapper.classList.toggle("active");
+});
+
+var swiper = new Swiper(".mySwiper", {
+  grabCursor: true,
+  centeredSlides: false,
+  spaceBetween: 30,
+  autoplay: {
+    delay: 2500,
+    disableOnInteractoin: false,
+  },
+  slidesPerView: "auto",
+  coverflowEffect: {
+    rotate: 0,
+    stretch: 0,
+    depth: 0,
+    modifier: 1,
+    slideShadows: true,
+  },
+  navigation: {
+    nextEl: ".bxs-chevron-right-circle",
+    prevEl: ".bxs-chevron-left-circle",
+  },
+});
+
+var swiper = new Swiper(".mySwiperTesti", {
+  pagination: {
+    el: ".swiper-pagination",
+    clickable: true,
+  },
+  autoplay: {
+    delay: 2500,
+    disableOnInteraction: false,
+  },
+});
