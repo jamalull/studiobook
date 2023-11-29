2aab04ff75e4e0a2ab222f06ef0b1d676ea8f288
jamalull <jamaluldazzled@gmail.com>
2023-11-29 22:44:31 +0700

    update and add new controller

--- controllers/studioController.js
+++ controllers/studioController.js
@@ -1,5 +1,7 @@
 const { PrismaClient } = require("@prisma/client");
+const multer = require("multer");
 
+//This page for store all functions that controll Studio in dashboard/backsite.
 class StudioController {
   constructor() {
     this.prisma = new PrismaClient();
@@ -7,20 +9,55 @@ class StudioController {
 
   getStudio = async (req, res) => {
     const studios = await this.prisma.studio.findMany();
-    const users = await this.prisma.user.findMany();
+    // const users = await this.prisma.user.findMany();
 
-    res.render("pages/admin/studioPage", { studios, users, user: req.user });
+    res.render("pages/admin/studioPage", { studios, user: req.user });
   };
 
   createNewStudio = async (req, res) => {
     try {
-      const payload = req.body;
-      payload.ownerId = parseInt(payload.ownerId);
-      payload.room = parseInt(payload.room);
-      payload.price = parseInt(payload.price);
-      await this.prisma.studio.create({
-        data: payload,
+      let storage = multer.diskStorage({
+        destination: (req, file, callback) => {
+          callback(null, "./upload");
+        },
+        filename: (req, file, callback) => {
+          let temp_file_arr = file.originalname.split(".");
+          let temp_file_name = temp_file_arr[0];
+          let temp_file_extension = temp_file_arr[1];
+          callback(
+            null,
+            temp_file_name + "-" + Date.now() + "." + temp_file_extension
+          );
+        },
       });
+    
+      let upload = multer({ storage: storage }).single("image");
+      upload(req, res, async (error) => {
+        const payload = req.body;
+        payload.ownerId = parseInt(payload.ownerId);
+        payload.room = parseInt(payload.room);
+        payload.price = parseInt(payload.price);
+        
+        if (error) {
+          return res.end("Error Uploading File");
+        } else {
+          console.log("Body ", payload);
+          console.log("File ", req.file);
+          // await this.prisma.studio.create({
+          //   data: {
+          //     ownerId: payload.ownerId,
+          //     image: req.file.filename,
+          //     name: payload.name,
+          //     description: payload.description,
+          //     address: payload.address,
+          //     price: payload.price,
+          //     category: payload.category,
+          //     room: payload.room,
+          //   },
+          // });
+        }
+      });
+      
       res.redirect(`/dashboard/studios`);
     } catch (error) {
       res.send(error.message);
@@ -65,7 +102,7 @@ class StudioController {
       });
 
       if (!studio) {
-        res.send("Studio not found");
+        res.status(404).send("Studio not found");
       }
 
       await this.prisma.studio.delete({
