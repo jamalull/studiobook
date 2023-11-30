19f9b8a8ac321d86923423809f05e114db044c1e
jamalull <jamaluldazzled@gmail.com>
2023-11-30 22:05:36 +0700

    update routes

--- routes/frontSiteRoutes.js
+++ routes/frontSiteRoutes.js
@@ -9,6 +9,7 @@ router.get("/studios3", FrontSiteController.getAllListStudio3);
 router.get("/studios/detail-studio/:studioId", FrontSiteController.getDetailStudio);
 router.get("/dashboard", isLoggedIn, FrontSiteController.getDashboard);
 router.get("/my-reservations", FrontSiteController.getHistoryReservations);
+router.get("/success-payment", FrontSiteController.getSuccessPayment);
 
 router.post("/detail-reservation", FrontSiteController.getReservationPage);
 router.get("/detail-ticket/:reservationId", FrontSiteController.getDetailTiketPage);
@@ -16,5 +17,6 @@ router.post("/create-reservation", FrontSiteController.createReservation);
 // router.get("/payment", FrontSiteController.createReservation);
 router.get("/payment/:transactionId", FrontSiteController.getPaymentPage);
 router.post("/payment", FrontSiteController.createPayment);
+router.post("/completeReservationbyCustomer", FrontSiteController.completeReservation);
 
 module.exports = router;
--- routes/reservationRoutes.js
+++ routes/reservationRoutes.js
@@ -1,7 +1,8 @@
 const router = require("express").Router();
-// const isLoggedIn = require("../middleware/isLoggedIn");
-// const ReservationController = require("../controllers/reservationController");
+const isLoggedIn = require("../middleware/isLoggedIn");
+const ReservationController = require("../controllers/reservationController");
 
-// router.get("/reservation", ReservationController.getReservationPage);
+router.get("/", isLoggedIn, ReservationController.getManageReservationPage);
+router.post("/updateStatusReservation", isLoggedIn, ReservationController.updateStatusReservation);
 
 module.exports = router;
